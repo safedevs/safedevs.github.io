57ef59693877ef652595834b953eb31e83f1e6e3
Samuel <samuel.venzi@me.com>
2020-08-30 19:42:56 -0300

    Make menu responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
-$(document).ready(function () {
-  // add padding top to show content behind navbar
-  $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
-  
-  // detect scroll top or down
-  if ($('.smart-scroll').length > 0) { // check if element exists
-      var last_scroll_top = 0;
-      $(window).on('scroll', function() {
-          scroll_top = $(this).scrollTop();
-          if(scroll_top < last_scroll_top) {
-              $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-          }
-          else {
-              $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-          }
-          last_scroll_top = scroll_top;
-      });
-  }
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
 });
+
+
+function checkWidth() {
+  if ($(window).width() > 800) {
+    $('#js-menu').removeClass('active');
+  }
+}
+
+$(window).resize(checkWidth);
